dfcaf913638585ff393991a3f110cd91dcffb199
AlibekNematov <85082765+AlibekNematov@users.noreply.github.com>
2021-12-12 02:50:41 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+    public static void main (String[] args) {
+        MovablePoint movablePoint = new MovablePoint(0,0,2,2);
+        MovablePoint prototype1 = new MovablePoint(0,0,0,0);
+        prototype1 = movablePoint.prototype();
+        System.out.println(movablePoint.toString());
+        movablePoint.moveRight();
+        System.out.println(movablePoint.toString());
+        movablePoint.moveUp();
+        System.out.println(movablePoint.toString());
+        movablePoint.moveRight();
+        System.out.println(movablePoint.toString());
+        System.out.println(prototype1.toString());
+
+
+        System.out.println("Movable circle: ");
+        MovableCircle movableCircle = new MovableCircle(0,0,3,1,4, movablePoint);
+        MovableCircle prototype2 = new MovableCircle(0,0,0,0,0,movablePoint);
+        prototype2 = movableCircle;
+        movableCircle.moveLeft();
+        System.out.println(movableCircle.toString());
+        movableCircle.moveLeft();
+        System.out.println(movableCircle.toString());
+        System.out.println(prototype2.toString());
+    }
+}
---
+++ Movable.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public interface Movable {
+    public MovablePoint prototype();
+
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
+}
